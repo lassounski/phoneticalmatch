96a8f5574c2a637718f2926214917eb166f48e10
Kirill Lassounski <kirillsk8@gmail.com>
2016-05-03 09:43:29 -0300

    adding the main class to the manifest.mf through the pom so it can be executed from the command line using java -jar

---
+++ src/main/java/com/delaru/foneticalmatch/Main.java
@@ -0,0 +1,22 @@
+package com.delaru.foneticalmatch;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author delaru
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+    
+}
