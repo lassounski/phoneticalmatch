189eee47bfb5c6fefa3a322855087f2ae1f2c650
Kirill Lassounski <kirillsk8@gmail.com>
2016-05-03 10:19:55 -0300

    fixing project name

--- src/main/java/com/delaru/phoneticalmatch/Main.java
+++ src/main/java/com/delaru/phoneticalmatch/Main.java
@@ -1,12 +1,7 @@
-package com.delaru.foneticalmatch;
+package com.delaru.phoneticalmatch;
 
 import java.util.Scanner;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 /**
  *
  * @author delaru
